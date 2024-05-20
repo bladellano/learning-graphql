67865a3bfdf96ea1e5d0a26db2ba1cbbef03e167
Caio <bladellano@gmail.com>
2024-05-20 15:16:24 +0100

    Tipos escalares

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
-const {gql, ApolloServer} = require("apollo-server")
+const { gql, ApolloServer } = require("apollo-server")
+
+/**
+ * Scalar Types
+ * - Int
+ * - Float
+ * - String
+ * - Boolean
+ * - ID 
+ *  */
+
+/**
+ * => Schema
+ * -> Schema Definition Language ou Linguagem de Definição de Esquema
+ * -> SDL
+ */
 
 const resolvers = {
     Query: {
-        hello () {
-            return 'World';
+        id() {
+            return 123456789
+        },
+        idade() {
+            return 18
+        },
+        salario() {
+            return 750.50
+        },
+        nome() {
+            return 'GraphQL'
+        },
+        ativo() {
+            return true
+        },
+        tecnologias() {
+            return ['GraphQL', 'PHP', 'Javascript', 8]
         }
     }
 }
- 
+
 const typeDefs = gql`
     type Query {
-        hello: String
+        idade: String
+        salario: Float
+        nome: String
+        ativo: Boolean
+        id: ID,
+        tecnologias: [String!]!
     }
 `
 
