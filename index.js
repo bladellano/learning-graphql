198600b084e3541edc7f5e8999582a958e5c8c0c
Caio <bladellano@gmail.com>
2024-06-19 11:21:53 +0100

    :sparkles:Customizando mensagem de erro.

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const { ApolloServer } = require("apollo-server");
 const { typeDefs, resolvers } = require("./src/graphql");
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
+    formatError: (err) => {
+        if (err.message.startsWith('Usuario existente:')) {
+            return new Error(err.message);
+        }
+    }
 });
 
 server.listen().then(({ url }) => console.log(url));
--- src/graphql/modules/Usuario/resolvers.js
+++ src/graphql/modules/Usuario/resolvers.js
@@ -1,49 +1,53 @@
 const { db, perfis } = require("../../../db");
 
 function geradorDeId(lista) {
-  let novoId;
-  let ultimo = lista[lista.length - 1];
+    let novoId;
+    let ultimo = lista[lista.length - 1];
 
-  if (!ultimo) {
-    novoId = 0;
-  } else {
-    novoId = ultimo.id;
-  }
+    if (!ultimo) {
+        novoId = 0;
+    } else {
+        novoId = ultimo.id;
+    }
 
-  return ++novoId;
+    return ++novoId;
 }
 
 module.exports = {
-  Usuario: {
-    salario(_) {
-      return _.salario_liquido;
+    Usuario: {
+        salario(_) {
+            return _.salario_liquido;
+        },
+        perfil(usuario) {
+            return perfis.find((p) => p.id === usuario.perfil);
+        },
     },
-    perfil(usuario) {
-      return perfis.find((p) => p.id === usuario.perfil);
+    Query: {
+        usuario(_, args) {
+            return db.find((db) => db.id === args.id);
+        },
+        usuarios: () => db,
     },
-  },
-  Query: {
-    usuario(_, args) {
-      return db.find((db) => db.id === args.id);
-    },
-    usuarios: () => db,
-  },
-  Mutation: {
-    criarUsuario(_, args) {
-      const { email } = args;
+    Mutation: {
+        criarUsuario(_, args) {
+            const { email } = args;
 
-      const usuarioExistente = db.some((u) => u.email === email);
+            const usuarioExistente = db.some(u => u.email === email);
 
-      const novoUsuario = {
-        ...args,
-        id: geradorDeId(db),
-        perfil: 2,
-      };
+            if (usuarioExistente) {
+                throw new Error(`Usuario existente: ${args.nome}`);
+            }
 
-      db.push(novoUsuario);
-      return novoUsuario;
+            const novoUsuario = {
+                ...args,
+                id: geradorDeId(db),
+                perfil: 2,
+            };
+
+            db.push(novoUsuario);
+            return novoUsuario;
+        },
     },
-  },
 };
 
 // console.log(geradorDeId(db))
